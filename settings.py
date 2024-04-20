1c5bd7de0076229079d9012101c26fc04daace75
Will Hedges <wshedges@outlook.com>
2024-04-20 15:45:07 -0500

    Initial screen, ship placement

---
+++ settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# settings.py - settings for alien_invasion.py
+
+class Settings:
+    """Main settings class for alien_invasion.py"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
